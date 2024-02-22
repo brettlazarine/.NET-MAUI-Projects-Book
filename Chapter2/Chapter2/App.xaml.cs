6c30aca397535587dd88ddde7062eada509569a4
Brett Lazarine <jblazarine@gmail.com>
2024-02-22 07:34:03 -0600

    init commit

---
+++ Chapter2/Chapter2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Chapter2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
